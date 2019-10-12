2875b4c24c2c45a4dc3a051af5fa5d74c3ab24c5
Colin Su <shaochuan.su@xjlit.com>
2019-10-12 10:09:13 +0800

    低1

---
+++ src/Hello.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dell
+ * Date: 2019/10/12
+ * Time: 10:07
+ */
+namespace sushao;
+class Hello
+{
+	public function abc(){
+		echo "helloworld";
+	}
+}
